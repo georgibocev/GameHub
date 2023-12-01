ecf0b23dfffb4998930209667599d28c56c6930c
Георги Боцев <130504275+georgibocev@users.noreply.github.com>
2023-12-01 17:59:03 +0200

    Add files via upload

---
+++ build/RGBTask/CMakeFiles/RGBTaskLib.dir/lib/longest_sequence_lib.c.obj.d
@@ -0,0 +1,21 @@
+RGBTask/CMakeFiles/RGBTaskLib.dir/lib/longest_sequence_lib.c.obj: \
+ E:\CODEACADEMY_GIT\s05-Codename-Kids-Next-Door\RGBTask\lib\longest_sequence_lib.c \
+ E:\CODEACADEMY_GIT\s05-Codename-Kids-Next-Door\RGBTask\lib\longest_sequence_lib.h \
+ E:/MSYS2/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stdbool.h \
+ E:/MSYS2/mingw64/include/stdio.h \
+ E:/MSYS2/mingw64/include/corecrt_stdio_config.h \
+ E:/MSYS2/mingw64/include/corecrt.h E:/MSYS2/mingw64/include/_mingw.h \
+ E:/MSYS2/mingw64/include/_mingw_mac.h \
+ E:/MSYS2/mingw64/include/_mingw_secapi.h \
+ E:/MSYS2/mingw64/include/vadefs.h \
+ E:/MSYS2/mingw64/include/sdks/_mingw_ddk.h \
+ E:/MSYS2/mingw64/include/_mingw_off_t.h \
+ E:/MSYS2/mingw64/include/swprintf.inl \
+ E:/MSYS2/mingw64/include/sec_api/stdio_s.h \
+ E:/MSYS2/mingw64/include/stdlib.h \
+ E:/MSYS2/mingw64/include/corecrt_wstdlib.h \
+ E:/MSYS2/mingw64/include/limits.h E:/MSYS2/mingw64/include/crtdefs.h \
+ E:/MSYS2/mingw64/include/sec_api/stdlib_s.h \
+ E:/MSYS2/mingw64/include/malloc.h \
+ E:/MSYS2/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/mm_malloc.h \
+ E:/MSYS2/mingw64/include/errno.h
